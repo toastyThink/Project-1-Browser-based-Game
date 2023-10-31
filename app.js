427ea4141a74951ce6597cb51943554dfcadb638
Brendan <shaefferbrendan@gmail.com>
2023-10-30 23:54:25 -0400

    Developmemt Phase 7

--- app.js
+++ app.js
@@ -10,12 +10,35 @@ let spinTime = true;
 let luckyNums = []; 
 let stopBtnArr = [];
 
+const reelSpin = [
+    {transform: "rotate(0)"},
+    {transform: "rotate(360deg)"}
+];
+
+const reel1 = document.querySelector('#reel1');
+const reel2 = document.querySelector('#reel2');
+const reel3 = document.querySelector('#reel3');
 const stopBtn = document.querySelector('#stopper');
+const playerMessage = document.querySelector('h1');
+const balance = document.querySelector('h2 > span');
 
 document.addEventListener('click', function(stopBtn){
     stopBtnArr.unshift('X'); 
     stopper();
     console.log(stopBtnArr);
+
+//Diamonds: if( luckyNums[0] % 9 = 0 && luckyNums[1] % 9 = 0 && luckyNums[2] % 9 = 0){
+    // playerMessage.innerText = 'Diamonds are everyone's best friend! You win $9,000!!'
+// balance.innerText + 9,000;
+//}
+
+
+
+
+
+
+
+
 });
 
 document.addEventListener('DOMContentLoaded', (Event) => {
@@ -23,19 +46,24 @@ document.addEventListener('DOMContentLoaded', (Event) => {
     //cycling through randomly generated numbers
         init();
     //animation of reels immediatly begins playing 
-    
-    //function reelsSpin(){
-
-    //}
 });
 
 function init() {
     if(spinTime === true){
        timer = setInterval(generateNum, interval);
-    }
+        reel1.animate([{transform: "rotate(0)"}, {transform: "rotate(360deg)"}], 
+        {duration: 1000, iterations: Infinity});
+        reel2.animate([{transform: "rotate(0)"}, {transform: "rotate(360deg)"}], 
+        {duration: 1000, iterations: Infinity});
+        reel3.animate([{transform: "rotate(0)"}, {transform: "rotate(360deg)"}], 
+        {duration: 1000, iterations: Infinity});
+        
+    }   
     if(spinTime === false){
-        console.log(spinTime);
-        console.log(stopBtnArr);
+
+        reel1.removeAttribute('animate');
+        
+        
     }
     return;
 }
@@ -59,6 +87,7 @@ function stopper(){
         //make spinning animation stop the reel on the matching face
         //according to the random number generated
         console.log('false');
+        console.log(reel1);
     }
     if(stopBtnArr.length % 2 === 0){
          //Begin the game again. Resume generating numbers
